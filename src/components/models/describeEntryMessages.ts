38898cc76d12a7878ba8c71f98407ff339871ea9
Athul Narayanan <narayanan@algomau.ca>
2026-04-14 16:09:01 -0400

    Document integration in describe individuals

---
+++ src/components/models/describeEntryMessages.ts
@@ -0,0 +1,23 @@
+export const DESCRIBE_COLUMN_HEADER = "Describe";
+export const DESCRIBE_DIALOG_FALLBACK_TITLE = "Description";
+export const DESCRIBE_DIALOG_LOADING_TEXT = "Generating...";
+export const DESCRIBE_DIALOG_EMPTY_TEXT = "No text";
+export const DESCRIBE_DIALOG_AUDIO_TIP =
+  "Tip: Use the speaker button to listen to this narrative.";
+export const DESCRIBE_DIALOG_AUDIO_LOADING_LABEL = "Generating audio";
+export const DESCRIBE_DIALOG_PLAY_LABEL = "Play audio";
+export const DESCRIBE_DIALOG_PAUSE_LABEL = "Pause audio";
+export const DESCRIBE_DIALOG_CLOSE_LABEL = "Close";
+export const DESCRIBE_DIALOG_GENERATE_FIRST_TITLE = "Generate text first";
+export const DESCRIBE_DIALOG_PLAY_TITLE = "Play";
+export const DESCRIBE_DIALOG_PAUSE_TITLE = "Pause";
+export const DESCRIBE_DEFAULT_PERSON_LABEL = "Individual";
+export const DESCRIBE_ERROR_TEXT =
+  "Something went wrong while generating the description.";
+export const DESCRIBE_DOCUMENTS_SECTION_TITLE = "Associated documents";
+export const DESCRIBE_DOCUMENTS_SECTION_TEXT =
+  "If you would like to review records connected with this individual, you can open the associated documents here.";
+export const DESCRIBE_DOCUMENTS_SECTION_NOTE =
+  "The document viewer will show any files currently linked to this person.";
+export const DESCRIBE_DOCUMENTS_BUTTON_LABEL = "View Associated Documents";
+export const DESCRIBE_DOCUMENTS_LOADING_LABEL = "Opening documents...";
